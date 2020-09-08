a46fbae373d99cc35e39e12f6a9c4faa5845dd87
NybuTG <derk.jan.weening@gmail.com>
2020-09-08 14:56:59 +0200

    File Sorting + Some JS code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 function whoAmI()
 {
-	location.replace("whoami.html")
+	location.replace("sub/whoami.html")
 }
+
+
+function myHobbies()
+{
+	location.replace("sub/hobbies.html")
+}
+
+function myAnimals()
+{
+	location.replace("sub/animals.html")
+}
+
+function mySchool()
+{
+	location.replace("sub/school.html")
+}
+
